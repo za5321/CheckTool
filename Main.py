cf324e12402cbe92263bf65c416c27cb75da48de
za5321 <za5321@naver.com>
2022-10-27 17:56:25 +0900

    Changed TodaysChecker's ui and allowed the system to use NanumGothic font.

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
 from main.TodaysChecker import TodaysChecker
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont, QFontDatabase
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    font_db = QFontDatabase()
+    font_db.addApplicationFont('font\\NanumGothic.ttf')
+    app.setFont(QFont('NanumGothic', 10))
+
     w = TodaysChecker()
     sys.exit(app.exec())
--- main/TodaysChecker.py
+++ main/TodaysChecker.py
@@ -1,19 +1,31 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QAbstractButton
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
 
 
 class TodaysChecker(QDialog):
     def __init__(self, parent=None):
         from PyQt5 import uic
-        from PyQt5.QtGui import QIcon
 
         super(TodaysChecker, self).__init__(parent)
 
         self.ui = uic.loadUi("ui\TodaysChecker.ui")
-        self.ui.setWindowTitle(self.get_config("window_title"))
-        self.ui.setWindowIcon(QIcon(self.get_config("icon")))
+        self.set_ui()
         self.ui.show()
-
         self.ui.buttonBox.accepted.connect(self.accepted)
+
+    def set_ui(self):
+        self.ui.setWindowTitle(self.get_config("window_title"))
+        self.ui.setWindowIcon(QtGui.QIcon(self.get_config("icon")))
+        font_db = QtGui.QFontDatabase()
+        font_db.addApplicationFont('font\\NanumGothic.ttf')
+
+        self.ui.textBrowser.setFont(QtGui.QFont('NanumGothic', 11))
+        self.ui.lineEdit1.setFont(QtGui.QFont('NanumGothic', 10))
+
+        self.ui.textBrowser.setText("당직자")
+        self.ui.textBrowser.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
+        self.ui.lineEdit1.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
 
     @staticmethod
     def get_config(flag: str):
